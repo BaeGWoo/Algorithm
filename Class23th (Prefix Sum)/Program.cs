49c5938808e1944b385202a6159356fb9892e87b
BaeGWoo <gwbae4211@naver.com>
2024-07-31 14:09:18 +0900

    (2024.07.31) 누적합과 구간합

---
+++ Class23th (Prefix Sum)/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Class23th__Prefix_Sum_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 누적합
+            // 특정한 배열이 있을 때 해당 배열까지의 합을 의미합니다.
+
+            int[] arr = new int[] { 7, 3, 4, 5, 1 };
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] += arr[i - 1];
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int cur = arr[i];
+                if (i != 0)
+                    cur -= arr[i - 1];
+                Console.WriteLine("누적합 : " + arr[i] + " (Original -> " + cur + ")");
+            }
+
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -11,7 +11,8 @@ namespace Program
         static void Main(string[] args)
         {
 
-          
+         
+
 
         }
     }
